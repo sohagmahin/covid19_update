f7ddf6cccebb061ea746de708a8685d721b7da3b
Sohag <sohagmahin@gmail.com>
2020-06-26 17:17:55 +0600

    removed dependency methods

--- lib/provider/cases_provider.dart
+++ lib/provider/cases_provider.dart
@@ -1,23 +1,18 @@
-import 'dart:convert';
 import 'package:coronavirustracker/models/cases_cn.dart';
 import 'package:coronavirustracker/models/cases_wd.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
-
-const String urlTotalCases='https://corona.lmao.ninja/v2/all#';
-const String urlAllCntryCases ='https://corona.lmao.ninja/v2/countries';
+import 'data_repository.dart';
 
 class CasesProvider extends ChangeNotifier {
   bool _isLoading = true;
-  CasesWD _casesWD;
-  List<CasesCN> _countyCases = [];
+  DataRepository dataRepository;
 
-  List<CasesCN> get casesCn {
-    return _countyCases;
+  List<CasesCN> get countryCases {
+    return dataRepository.countyCases;
   }
 
-  CasesWD get casesWD {
-    return _casesWD;
+  CasesWD get worldCases {
+    return dataRepository.worldCases;
   }
 
   bool get loadingStatus {
@@ -29,25 +24,9 @@ class CasesProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future fetchTotalCases() async {
-    http.Response response;
-    response = await http.get(urlTotalCases);
-    Map map = json.decode(response.body);
-    _casesWD = CasesWD.fromJson(map);
-  }
-
-  Future fetchAllCountryCases() async {
-    http.Response response;
-    response = await http.get(urlAllCntryCases);
-    List list = json.decode(response.body);
-    list.forEach((country) {
-      _countyCases.add(CasesCN.fromJson(country));
-    });
-  }
-
   Future initialCall() async {
-    await fetchTotalCases();
-    await fetchAllCountryCases();
+    dataRepository = DataRepository();
+    await dataRepository.dataRepoInitialCall();
     _isLoading = false;
     notifyListeners();
   }
